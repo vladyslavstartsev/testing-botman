7427225000a656bd1893a49810898186ec119f0c
vagrant <vagrant@homestead>
2017-03-12 13:04:09 +0000

    changed botman routes

--- routes/botman.php
+++ routes/botman.php
@@ -6,4 +6,9 @@ $botman = resolve('botman');
 $botman->hears('test', function($bot){
     $bot->reply('hello!');
 });
+
+$botman->hears('foo', function ($bot) {
+    $bot->reply('Hello World');
+});
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
